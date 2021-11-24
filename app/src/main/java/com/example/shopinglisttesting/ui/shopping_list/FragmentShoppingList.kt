5ed1b45ef6ffd5977dce813defd1d5f6e96cad92
lubshad <lubshad4u4@gmail.com>
2021-11-25 00:12:32 +0530

    fragment and navgrap added

---
+++ app/src/main/java/com/example/shopinglisttesting/ui/shopping_list/FragmentShoppingList.kt
@@ -0,0 +1,8 @@
+package com.example.shopinglisttesting.ui.shopping_list
+
+import androidx.fragment.app.Fragment
+import com.example.shopinglisttesting.R
+
+class FragmentShoppingList: Fragment(R.layout.fragment_shoping_list) {
+
+}
