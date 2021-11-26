c689094c3d945d699c4e1290b35b8236aaaedbae
lubshad <lubshad4u4@gmail.com>
2021-11-26 21:01:20 +0530

    add item fragment in middel

---
+++ app/src/main/java/com/example/shopinglisttesting/ui/fragment_add_item/FragmentAddItem.kt
@@ -0,0 +1,12 @@
+package com.example.shopinglisttesting.ui.fragment_add_item
+
+import androidx.fragment.app.Fragment
+import com.example.shopinglisttesting.R
+
+
+@AndroidEntryPoint
+class FragmentAddItem:Fragment(R.layout.fragment_add_new_item) {
+
+
+
+}
---
+++ app/src/main/java/com/example/shopinglisttesting/ui/fragment_add_item/FragmentAddItemViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.shopinglisttesting.ui.fragment_add_item
+
+import androidx.lifecycle.ViewModel
+
+
+@HiltViewModel
+class FragmentAddItemViewModel: ViewModel() {
+
+}
